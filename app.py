6c7bcfca3a4e965831a451f0169c21d68adb2778
Charles Yoon <jyy24@sinkhole.srcf.net>
2018-02-09 20:31:08 +0000

    Adapt for Server

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask, request, g, current_app
+from werkzeug.local import LocalProxy
+from ibisclient import *
+
+app = Flask(__name__)
+
+def getIbis():
+    if getattr(g, 'conn', None) is None:
+        g.conn = createTestConnection()
+    if getattr(g, 'methods', None) is None:
+        g.methods = PersonMethods(g.conn)
+    return g.methods
+
+methods = LocalProxy(getIbis)
+
+@app.route('/')
+@app.route('/index.html')
+def index():
+    crsid = request.headers.get('X-Aaprincipal').split(' ')[1]
+    person = methods.getPerson("crsid", crsid, fetch="all_insts")
+    instid_list = list()
+    for i in person.institutions:
+        instid_list.append(i.instid)
+    return "{crsid} at {inst}".format(crsid=crsid, inst=str(instid_list))
+
+
