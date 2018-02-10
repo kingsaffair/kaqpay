7df194cccf200ccda9a19bf28feee5e86fe4cc99
Charles J.Y Yoon <1242029+jaeyeun97@users.noreply.github.com>
2018-02-10 00:17:39 +0000

    Sample Config & App Feature Done

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
-from flask import Flask, request, g, current_app
+from flask import Flask, request, g, current_app, redirect
 from werkzeug.local import LocalProxy
 from ibisclient import *
+from functools import reduce
+from urllib import parse
+import jwt
 
 app = Flask(__name__)
+app.config.from_object('config')
 
 def getIbis():
     if getattr(g, 'conn', None) is None:
@@ -16,15 +20,29 @@ methods = LocalProxy(getIbis)
 @app.route('/')
 @app.route('/index.html')
 def index():
-    crsid = request.headers.get('X-Aaprincipal').split(' ')[1]
+    # crsid = request.headers.get('X-Aaprincipal').split(' ')[1]
+    crsid = 'jyy24'
     person = methods.getPerson("crsid", crsid, fetch="all_insts")
-    instid_list = list()
-    for i in person.institutions:
-        instid_list.append(i.instid)
-    if 'KINGSUG' in instid_list or 'KINGSPG' in instid_list:
-        return "King's Member!"
+    instids = list(map(lambda i: i.instid, person.institutions))
+    is_kings = reduce(lambda x, y: x or y, [(i in instids) for i in app.config.get('KINGS')])
+    payload = {'email': "{}@cam.ac.uk".format(crsid), 'kings': is_kings}
+    encoded = jwt.encode(payload, app.config.get('JWT_KEY'), algorithm=app.config.get('JWT_ALGORITHM')) 
+    return redirect("{qpay}?{enc}".format(qpay=app.config.get('QPAY_URL'), 
+                                          enc=parse.urlencode({'jwt': encoded})))
+
+
+@app.route('/test_response')
+def test_response():
+    param = request.args.get('jwt')
+    if param is not None:
+        enc = parse.unquote(param)
+        # The following only works with symetric ones.
+        print(enc)
+        result = jwt.decode(enc, key=app.config.get('JWT_KEY'), algorithms=[app.config.get('JWT_ALGORITHM')])
+        return str(result)
     else:
-        return "Not a King's Member."
-        
+        return "Pass in a JWT."
+    
 
-
+if __name__ == "__main__":
+    app.run()
